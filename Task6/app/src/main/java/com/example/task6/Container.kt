91cbe2d01ebefc3273721d456b2e67a9a42c247d
vladimirlisovskij <vladimir.lisovskij.2002@gmail.com>
2021-04-11 18:21:05 +0300

    small fix

--- Task6/app/src/main/java/com/example/task6/Container.kt
+++ Task6/app/src/main/java/com/example/task6/Container.kt
@@ -6,25 +6,5 @@ import kotlinx.parcelize.Parceler
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class Container(val imageLink: String, val name: String, val phone: String, val email: String, val id: String) : Parcelable{
-
-    constructor(parcel: Parcel) : this(
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: "",
-            parcel.readString() ?: ""
-    )
-
-    companion object : Parceler<Container> {
-        override fun Container.write(parcel: Parcel, flags: Int) {
-            parcel.writeString(imageLink)
-            parcel.writeString(name)
-            parcel.writeString(phone)
-            parcel.writeString(email)
-            parcel.writeString(id)
-        }
-
-        override fun create(parcel: Parcel): Container = Container(parcel)
-    }
-}
+class Container(val imageLink: String, val name: String, val phone: String, val email: String, val id: String)
+    : Parcelable
